10e3427d1fe35837092d953024c6cf15dffd1a52
loregbrw <loregobara@gmail.com>
2024-01-05 14:41:45 -0300

    finished LinkedList

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String[] args) {
+        var arr = new LinkedList<Integer>();
 
+        arr.addLast(1);
+        arr.addLast(3);
+
+        System.out.println(arr.getLast());
+        System.out.println(arr.getFirst());
   }
 }
